15cc3a27ac3891970eb80f0d9f5aa8369099f7be
joseminelli <zecarminelli@gmail.com>
2025-05-28 20:09:16 -0300

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿// UsersController.cs
+using Microsoft.AspNetCore.Mvc;
+using blackfy_API.Models;
+using blackfy_API.Services; 
+
+namespace blackfy_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService userService;
+
+        public UsersController(UserService userService) 
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await userService.GetAllUsersAsync();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            var result = await userService.CreateUserAsync(user);
+
+            return result ? Ok("User criado!") : StatusCode(500, "Erro ao criar User...");
+        }
+    }
+}
---
+++ Service/UserService.cs
@@ -0,0 +1,44 @@
+﻿using Npgsql;
+using blackfy_API.Models;
+using Dapper;
+
+namespace blackfy_API.Services
+{
+    public class UserService 
+    {
+        private readonly IConfiguration config;
+
+        public UserService(IConfiguration configuration)
+        {
+            config = configuration;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            var connection = new NpgsqlConnection(connectionString);
+
+            var users = await connection.QueryAsync<User>("SELECT Id, Name, Email, Phone FROM Users");
+            return users;
+        }
+
+        public async Task<bool> CreateUserAsync(User user)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            var connection = new NpgsqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            var query = new NpgsqlCommand("INSERT INTO Users (Name, Email, Phone) VALUES (@name, @email, @phone)", connection);
+
+            query.Parameters.AddWithValue("name", user.Name);
+            query.Parameters.AddWithValue("email", user.Email);
+            query.Parameters.AddWithValue("phone", user.Phone);
+
+            var result = await query.ExecuteNonQueryAsync();
+
+            return result > 0;
+        }
+
+    }
+}
