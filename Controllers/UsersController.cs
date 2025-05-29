b7dcaec6003285635609773f798518bfbe1e8932
joseminelli <zecarminelli@gmail.com>
2025-05-29 15:23:22 -0300

    User data validation

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -19,30 +19,65 @@ namespace blackfy_API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var users = await userService.GetAllUsersAsync();
-            return Ok(users);
+            try { 
+                var users = await userService.GetAllUsersAsync();
+                return Ok(users);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
         }
 
         [HttpPost]
         public async Task<IActionResult> Create(User user)
         {
-            var result = await userService.CreateUserAsync(user);
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
 
-            return result ? Ok("User criado!") : StatusCode(500, "Erro ao criar User...");
+                var result = await userService.CreateUserAsync(user);
+
+                return result ? Ok("User criado!") : StatusCode(500, "Erro ao criar User...");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
         }
 
         [HttpDelete]
         public async Task<IActionResult> Delete(int id)
         {
-            var result = await userService.DeleteUserById(id);
-            return result ? Ok("User deletado!") : StatusCode(500, "Erro ao deletar User");
+            try
+            {
+                var result = await userService.DeleteUserById(id);
+                return result ? Ok("User deletado!") : StatusCode(500, "Erro ao deletar User");
+            } catch (Exception e)
+            {
+                return BadRequest(e);
+            }
         }
 
         [HttpPut]
         public async Task<IActionResult> Update(User user, int id)
         {
-            var result = await userService.UpdateUser(user, id);
-            return result ? Ok("User alterado!") : StatusCode(500, "Erro ao atualizar User");
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var result = await userService.UpdateUser(user, id);
+                return result ? Ok("User alterado!") : StatusCode(500, "Erro ao atualizar User");
+            } 
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
         }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,19 @@
-﻿namespace blackfy_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blackfy_API.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório.")]
         public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O email não está em um formato válido.")] 
         public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O telefone é obrigatório.")]
         public string Phone { get; set; } = string.Empty;
     }
 }
